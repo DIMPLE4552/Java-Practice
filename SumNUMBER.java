2c085161dc216021b32e673cdd5112648390ff7c
DIMPLE <128499208+DIMPLE4552@users.noreply.github.com>
2023-03-22 17:38:21 +0530

    Update SumNUMBER.java

--- SumNUMBER.java
+++ SumNUMBER.java
@@ -2,7 +2,8 @@ public class SumNUMBER {
     public static void main(String[] args){
         int n1 = 5;
         int n2 = 10;
-        int add=n1+n2;System.out.println(add);
+        int add=n1+n2;
+        System.out.println(add);
 
     }
     
